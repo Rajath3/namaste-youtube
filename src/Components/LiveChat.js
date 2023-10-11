1e87aeff89caa2ab6380fdc9a1447eb0aa9e3225
Rajath H R <rajath.nandan33@gmail.com>
2023-10-11 17:15:27 +0530

    Add live message to live chat

--- src/Components/LiveChat.js
+++ src/Components/LiveChat.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import LiveComment from './LiveComment'
 import { generateMessage, generateName } from '../utils/helper'
 import { useDispatch, useSelector } from 'react-redux'
@@ -6,7 +6,7 @@ import { addMessage } from '../utils/chatSlice'
 
 
 const LiveChat = () => {
-
+  const [liveMessage, setLiveMessage] = useState('')
   const dispatch = useDispatch();
 
   const chatMessages = useSelector(state => state.chat.message)
@@ -23,10 +23,24 @@ const LiveChat = () => {
     }
   }, [])
   return (
+    <>
     <div className='m-2 p-2 border border-black rounded-lg w-[34rem] h-[34rem] overflow-y-scroll flex flex-col-reverse'>
-        {chatMessages.map((val, i)=><LiveComment name={val.name} message={val.message}/>)}
+        {chatMessages.map((val, i)=><LiveComment key={i} name={val.name} message={val.message}/>)}
             
     </div>
+    <span className='font-bold px-6 mb-2'>LiveChat</span>
+    <form className='w-96 px-4 mx-2 border border-blue-100' onSubmit={(e)=>{
+        e.preventDefault();
+        dispatch(addMessage({
+            name: 'Rajath',
+            message: liveMessage
+        }))
+        setLiveMessage("")
+    }}>
+        <input className='border border-black-100 mx-2 p-4' type='text' value={liveMessage} onChange={(e)=> setLiveMessage(e.target.value)}/>
+        <button className='border border-black-100 bg-blue-200 rounded-lg p-2'>Send</button>
+    </form>
+    </>
   )
 }
 
